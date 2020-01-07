4cb667877da15501985b43df94d84d1048cb346c
duankai <747375074@qq.com>
2020-01-07 11:55:07 +0800

    1.7

--- webpack-test/src/index.js
+++ webpack-test/src/index.js
@@ -1,36 +1,23 @@
-/*
- * @Author: your name
- * @Date: 2020-01-02 14:10:30
- * @LastEditTime : 2020-01-03 10:41:56
- * @LastEditors  : Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \EVO\webpack-test\src\index.js
- */
-import _ from "lodash";
-import printMe from "./print.js";
+ import _ from 'lodash';
 
-// import './styles.css';
+ function component() {
+    var element = document.createElement('div');
+   var button = document.createElement('button');
+   var br = document.createElement('br');
 
-function component() {
-  let element = document.createElement("div");
-  var btn = document.createElement("button");
+   button.innerHTML = 'Click me and look at the console!';
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   element.appendChild(br);
+   element.appendChild(button);
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  btn.innerHTML = "点击这里，然后查看 1111console！";
-  btn.onclick = printMe;
-  element.appendChild(btn);
-  return element;
-}
+   // Note that because a network request is involved, some indication
+   // of loading would need to be shown in a production-level site/app.
+   button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+     var print = module.default;
 
-let element = component();
-document.body.appendChild(element);
+     print();
+   });
 
-if (module.hot) {
-  module.hot.accept("./print.js", function() {
-    console.log("Accepting the updated printMe module!");
-    document.body.removeChild(element);
-    element = component(); // Re-render the "component" to update the click handler
-    element = component(); // 重新渲染 "component"，以便更新 click 事件处理函数
-    document.body.appendChild(element);
-  });
-}
+    return element;
+  }
+ document.body.appendChild(component());
